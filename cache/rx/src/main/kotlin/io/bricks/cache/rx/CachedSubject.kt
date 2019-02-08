5b04eae022c390c45092126ddb4d02072d41f2e1
Janek Góral <yaneq6@gmail.com>
2019-02-09 00:09:45 +0100

    Remove legacy modules

--- cache/rx/src/main/kotlin/io/bricks/cache/rx/CachedSubject.kt
+++
@@ -1,9 +0,0 @@
-package io.bricks.cache.rx
-
-import io.bricks.cache.core.CachedProperty
-import io.reactivex.subjects.BehaviorSubject
-
-fun <T> BehaviorSubject<T>.cached(cachedProperty: CachedProperty<T>, default: (() -> T)? = null) = apply {
-    (cachedProperty.value ?: default?.invoke())?.let { onNext(it) }
-    subscribe { cachedProperty.value = it }
-}
